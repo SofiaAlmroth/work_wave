3d4881a343f0d348112f9caabb772675661845cb
sofia_almroth <almrothsogfia@gmail.com>
2024-05-10 13:14:09 +0200

    finished application

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className="navbar bg-base-100 ">
-      <Link to={"/users"} className="btn btn-ghost text-3xl">
-        Intensive Contacts
+      <Link to={"/users"} className="btn btn-ghost text-4xl font-black">
+        Work Wave
       </Link>
     </div>
   );
