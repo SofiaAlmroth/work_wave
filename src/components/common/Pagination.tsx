61530d61065aa9a5f21f3cd3b11f54975280897c
sofia_almroth <almrothsogfia@gmail.com>
2025-07-11 11:47:58 +0200

    fixed pagination css

--- src/components/common/Pagination.tsx
+++ src/components/common/Pagination.tsx
@@ -12,28 +12,28 @@ function Pagination({
   onPageSelect,
 }: Props) {
   const pageCount = Math.ceil(totalCount / pageSize);
-  if (pageCount <= 1) return null;
+
+  const isFirst = selectedPage === 1;
+  const isLast = selectedPage === pageCount;
+  const isSinglePage = pageCount <= 1;
 
   return (
     <div className="join h-10 ">
       <button
         onClick={() => onPageSelect(selectedPage - 1)}
-        className={`join-item btn btn-sm h-10 w-10 text-white bg-primary hover:bg-primary/85 hover:scale-105 transition-all duration-200"
-        ${
-          selectedPage === 1
-            ? "bg-primary/40 cursor-not-allowed"
-            : "bg-primary hover:bg-primary/80 hover:scale-105"
-        }`}
+        disabled={isFirst || isSinglePage}
+        className="join-item btn btn-sm h-10 w-10 text-white transition-all duration-200 bg-primary hover:bg-primary/80 hover:scale-105 disabled:bg-primary/40 disabled:cursor-not-allowed disabled:hover:scale-100"
       >
         «
       </button>
-      <button className="join-item btn btn-sm h-10 w-10 text-white bg-primary hover:bg-primary ">
+
+      <button className="join-item btn btn-sm h-10 w-10 text-white bg-primary cursor-default">
         {selectedPage}
       </button>
       <button
         onClick={() => onPageSelect(selectedPage + 1)}
-        className="join-item btn btn-sm h-10 w-10 text-white bg-primary  hover:bg-primary/85 hover:scale-105 transition-all duration-200"
-        disabled={selectedPage === pageCount}
+        disabled={isLast || isSinglePage}
+        className="join-item btn btn-sm h-10 w-10 text-white transition-all duration-200 bg-primary hover:bg-primary/80 hover:scale-105 disabled:bg-primary/40 disabled:cursor-not-allowed disabled:hover:scale-100"
       >
         »
       </button>
